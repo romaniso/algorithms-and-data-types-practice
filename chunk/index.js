7595f56fbb64635a8f67a41ca294bec3d947fb81
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-07 21:42:15 +0200

    feat: add first solution for chunk exercise

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,12 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const outputArr = [];
+  while (array.length) {
+    outputArr.push([...array.splice(0, size)]);
+  }
+  return outputArr;
+}
 
 module.exports = chunk;
