ff898001a84c62d75de7f315f69a8e82998ef7d3
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-08 13:53:21 +0200

    feat: add third solution for capitalize task

--- capitalize/index.js
+++ capitalize/index.js
@@ -13,14 +13,27 @@
 //    .map((word) => word[0].toUpperCase() + word.slice(1))
 //    .join(" ");
 //}
-function capitalize(str) {
-  const words = [];
+//function capitalize(str) {
+//  const words = [];
 
-  for (let word of str.split(" ")) {
-    words.push(word[0].toUpperCase() + word.slice(1));
+//  for (let word of str.split(" ")) {
+//    words.push(word[0].toUpperCase() + word.slice(1));
+//  }
+
+//  return words.join(" ");
+//}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
 
-  return words.join(" ");
+  return result;
 }
 
 module.exports = capitalize;
