03803b0f46d374b17d75a5a2f3682e3256f4ac90
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-08 12:29:49 +0200

    feat: add first solution for anagrams task

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  function createMap(string) {
+    let map = {};
+    for (const char of string) {
+      if (!map[char]) {
+        map[char] = 1;
+      } else {
+        map[char]++;
+      }
+    }
+    return map;
+  }
+  function processStr(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase();
+  }
+
+  const stringAMap = createMap(processStr(stringA));
+  const stringBMap = createMap(processStr(stringB));
+
+  let isAnagram = true;
+
+  if (Object.keys(stringAMap).length !== Object.keys(stringBMap).length)
+    return false;
+  for (const char in stringAMap) {
+    if (stringAMap[char] !== stringBMap[char]) isAnagram = !isAnagram;
+  }
+
+  return isAnagram;
+}
+
+//anagrams("rail safety", "fairy tales");
+anagrams("A tree, a life, a bench", "A tree, a fence, a yard");
 
 module.exports = anagrams;
