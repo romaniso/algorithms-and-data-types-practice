1422a5a8964060b7b240ba46820852d14c0e891a
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-10 12:14:23 +0200

    feat: add second solution for pyramid task

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,33 +14,47 @@
 //       ' ### '
 //       '#####'
 
+//function pyramid(n) {
+//  for (let row = 0; row < n; row++) {
+//    let stair = "";
+//    const cols = 2 * n - 1;
+//    const middleOfRow = Math.floor(cols / 2);
+//    const hashNumber = row * 2 + 1;
+//    let leftHashes = hashNumber;
+//    for (let col = 0; col < cols; col++) {
+//      const stepsForwardBackward = Math.floor(hashNumber / 2);
+//      if (leftHashes === 1 && col === middleOfRow) {
+//        stair += "#";
+//        leftHashes--;
+//      } else if (
+//        leftHashes &&
+//        col >= middleOfRow - stepsForwardBackward &&
+//        col <= middleOfRow + stepsForwardBackward
+//      ) {
+//        stair += "#";
+//        leftHashes--;
+//      } else {
+//        stair += " ";
+//      }
+//    }
+//    console.log(stair);
+//  }
+//}
+
 function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
   for (let row = 0; row < n; row++) {
-    let stair = "";
-    const cols = 2 * n - 1;
-    const middleOfRow = Math.floor(cols / 2);
-    const hashNumber = row * 2 + 1;
-    let leftHashes = hashNumber;
-    for (let col = 0; col < cols; col++) {
-      const stepsForwardBackward = Math.floor(hashNumber / 2);
-      if (leftHashes === 1 && col === middleOfRow) {
-        stair += "#";
-        leftHashes--;
-      } else if (
-        leftHashes &&
-        col >= middleOfRow - stepsForwardBackward &&
-        col <= middleOfRow + stepsForwardBackward
-      ) {
-        stair += "#";
-        leftHashes--;
+    let level = "";
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
       } else {
-        stair += " ";
+        level += " ";
       }
     }
-    console.log(stair);
+    console.log(level);
   }
 }
-
-pyramid(4);
 
 module.exports = pyramid;
