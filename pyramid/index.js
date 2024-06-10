c0c817aa57b52dbd9d3c851ab8e64120e8bd9e07
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-10 11:12:38 +0200

    feat: add first solution for pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    const cols = 2 * n - 1;
+    const middleOfRow = Math.floor(cols / 2);
+    const hashNumber = row * 2 + 1;
+    let leftHashes = hashNumber;
+    for (let col = 0; col < cols; col++) {
+      const stepsForwardBackward = Math.floor(hashNumber / 2);
+      if (leftHashes === 1 && col === middleOfRow) {
+        stair += "#";
+        leftHashes--;
+      } else if (
+        leftHashes &&
+        col >= middleOfRow - stepsForwardBackward &&
+        col <= middleOfRow + stepsForwardBackward
+      ) {
+        stair += "#";
+        leftHashes--;
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+pyramid(4);
 
 module.exports = pyramid;
