16e0e4bbb66ba830a26d04b3dd48f1a19c9964eb
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-11 12:44:13 +0200

    feat: add first solution for vowels task

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let count = 0;
+  for (const char of str.split("")) {
+    for (const vowel of ["a", "e", "i", "o", "u"]) {
+      if (char.toLowerCase() === vowel) count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = vowels;
