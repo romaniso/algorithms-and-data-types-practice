e639d613be16b9cce3fa42fda135603e95cbedb1
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-11 12:13:41 +0200

    feat: add my recursive solution for pyramid task

--- pyramid/index.js
+++ pyramid/index.js
@@ -41,20 +41,37 @@
 //  }
 //}
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
+//function pyramid(n) {
+//  const midpoint = Math.floor((2 * n - 1) / 2);
 
-  for (let row = 0; row < n; row++) {
-    let level = "";
-    for (let column = 0; column < 2 * n - 1; column++) {
-      if (midpoint - row <= column && midpoint + row >= column) {
-        level += "#";
-      } else {
-        level += " ";
-      }
-    }
-    console.log(level);
+//  for (let row = 0; row < n; row++) {
+//    let level = "";
+//    for (let column = 0; column < 2 * n - 1; column++) {
+//      if (midpoint - row <= column && midpoint + row >= column) {
+//        level += "#";
+//      } else {
+//        level += " ";
+//      }
+//    }
+//    console.log(level);
+//  }
+//}
+
+function pyramid(n, row = 0, col = 0, level = "") {
+  if (row === n) {
+    return;
   }
+  if (col === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1, 0, "");
+  }
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (midpoint - row <= col && midpoint + row >= col) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+  pyramid(n, row, col + 1, level);
 }
 
 module.exports = pyramid;
