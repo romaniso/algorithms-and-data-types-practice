674e124ecd6e1353ccf9fb7fda3bb9b9f6789dac
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-11 13:00:42 +0200

    feat: add regex solution for vowels task

--- vowels/index.js
+++ vowels/index.js
@@ -18,16 +18,21 @@
 //  return count;
 //}
 
-function vowels(str) {
-  let counter = 0;
-  const checker = ["a", "e", "i", "o", "u"];
+//function vowels(str) {
+//  let counter = 0;
+//  const checker = ["a", "e", "i", "o", "u"];
 
-  for (const char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      counter++;
-    }
-  }
-  return counter;
+//  for (const char of str.toLowerCase()) {
+//    if (checker.includes(char)) {
+//      counter++;
+//    }
+//  }
+//  return counter;
+//}
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
