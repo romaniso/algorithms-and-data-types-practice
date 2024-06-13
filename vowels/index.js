33ce5af415b90233accdb6c394a25a42dde1c0f5
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-13 23:18:43 +0200

    refactor: refactor the first solution for vowels task

--- vowels/index.js
+++ vowels/index.js
@@ -7,16 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-//function vowels(str) {
-//  let count = 0;
-//  for (const char of str.split("")) {
-//    for (const vowel of ["a", "e", "i", "o", "u"]) {
-//      if (char.toLowerCase() === vowel) count++;
-//    }
-//  }
+function vowels(str) {
+  let count = 0;
+  for (const char of str.toLowerCase()) {
+    for (const vowel of ["a", "e", "i", "o", "u"]) {
+      if (char === vowel) count++;
+    }
+  }
 
-//  return count;
-//}
+  return count;
+}
 
 //function vowels(str) {
 //  let counter = 0;
