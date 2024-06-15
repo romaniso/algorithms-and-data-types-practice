e9a5357cb4a741b9490c99b210995c6bd2648720
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-15 13:40:58 +0200

    feat: add first solution for matrix task

--- matrix/index.js
+++ matrix/index.js
@@ -15,6 +15,51 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const last = n * n;
+  //  let next = 1;
+
+  //prepare an array of arrays
+  const matrix = [];
+  for (let i = 0; i < n; i++) {
+    const insideArr = new Array(n);
+    matrix.push([...insideArr]);
+  }
+  let counter = 1;
+
+  let startCol = 0;
+  let endCol = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let i = startCol; i <= endCol; i++) {
+      matrix[startRow][i] = counter;
+      counter = counter + 1;
+    }
+    startRow = startRow + 1;
+    //down
+    for (let i = startRow; i <= endRow; i++) {
+      matrix[i][endRow] = counter;
+      counter = counter + 1;
+    }
+    //left
+    endCol = endCol - 1;
+    for (let i = endCol; i >= startCol; i--) {
+      matrix[endRow][i] = counter;
+      counter = counter + 1;
+    }
+    //up
+    endRow = endRow - 1;
+    for (let i = endRow; i >= startRow; i--) {
+      matrix[i][startCol] = counter;
+      counter = counter + 1;
+    }
+    startCol = startCol + 1;
+  }
+
+  console.log(matrix);
+  return matrix;
+}
 
 module.exports = matrix;
