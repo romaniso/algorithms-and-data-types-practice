1afc8f6de0b24039ab440d04aa7ef456fbacdc9e
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-20 17:35:40 +0200

    feat: add fib recursive solution

--- fib/index.js
+++ fib/index.js
@@ -21,17 +21,24 @@
 //  }
 //  return sequence[sequence.length - 1];
 //}
+//function fib(n) {
+//  const result = [0, 1];
+
+//  for (let i = 2; i <= n; i++) {
+//    const a = result[i - 1];
+//    const b = result[i - 2];
+
+//    result.push(a + b);
+//  }
+
+//  return result[n];
+//}
 function fib(n) {
-  const result = [0, 1];
-
-  for (let i = 2; i <= n; i++) {
-    const a = result[i - 1];
-    const b = result[i - 2];
-
-    result.push(a + b);
+  if (n < 2) {
+    return n;
   }
 
-  return result[n];
+  return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = fib;
