4ca362873d49105b2d0580a520f0991c10289b83
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-21 10:46:23 +0200

    feat: add memoized recursive solution

--- fib/index.js
+++ fib/index.js
@@ -33,6 +33,20 @@
 
 //  return result[n];
 //}
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
 function fib(n) {
   if (n < 2) {
     return n;
@@ -40,5 +54,7 @@ function fib(n) {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoize(fib);
 
 module.exports = fib;
