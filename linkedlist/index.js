c5385a205ceca003d716ec90cdd73cf5bb17f708
Roman Isopenko <isopenkoroman@gmail.com>
2024-06-27 21:58:41 +0200

    feat: add removeFirst to LinkedList

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -44,6 +44,12 @@ class LinkedList {
   clear() {
     this.head = null;
   }
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
 }
 
 module.exports = { Node, LinkedList };
