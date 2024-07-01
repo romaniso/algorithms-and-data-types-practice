0ff0cfadf7d640f13bcf1a36a5893c319994a9fe
Roman Isopenko <isopenkoroman@gmail.com>
2024-07-01 09:12:12 +0200

    refactor: refactor loop iteration for fromLast

--- fromlast/index.js
+++ fromlast/index.js
@@ -15,15 +15,14 @@ function fromLast(list, n) {
   let slow = list.getFirst();
   let fast = list.getFirst();
 
-  for (let i = 0; i < n; i++) {
+  while (n > 0) {
     fast = fast.next;
+    n--;
   }
-
   while (fast.next) {
     slow = slow.next;
     fast = fast.next;
   }
-
   return slow;
 }
 
