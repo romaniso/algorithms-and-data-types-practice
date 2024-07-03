cb7c5455e8627a40060964b2a0cc245248d91b4d
Roman Isopenko <isopenkoroman@gmail.com>
2024-07-03 11:43:45 +0200

    feat: add insert method first solution

--- bst/index.js
+++ bst/index.js
@@ -11,6 +11,27 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+  insert(data) {
+    if (data < this.data) {
+      if (!this.left) {
+        this.left = new Node(data);
+      } else {
+        this.left.insert(data);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new Node(data);
+      } else {
+        this.right.insert(data);
+      }
+    }
+  }
+}
 
 module.exports = Node;
