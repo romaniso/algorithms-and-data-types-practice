eb77a78564e4d36a62e8a9366e62e3a6704df756
Roman Isopenko <isopenkoroman@gmail.com>
2024-07-07 11:17:00 +0200

    feat: add bubblesort

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,24 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      const current = arr[j];
+      const next = arr[j + 1];
+      if (current > next) {
+        arr[j] = next;
+        arr[j + 1] = current;
+      }
+    }
+  }
 
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
