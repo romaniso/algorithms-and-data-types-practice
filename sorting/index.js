0269b55d040cd07844728758d8c5fe2e7dc37574
Roman Isopenko <isopenkoroman@gmail.com>
2024-07-07 11:23:25 +0200

    refactor: refactor bubblesort

--- sorting/index.js
+++ sorting/index.js
@@ -3,12 +3,11 @@
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
-      const current = arr[j];
-      const next = arr[j + 1];
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (current > next) {
-        arr[j] = next;
-        arr[j + 1] = current;
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
       }
     }
   }
